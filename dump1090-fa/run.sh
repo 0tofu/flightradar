f2826062aa25fd8940c23cb11256463376431dcb
otofu <otofu.xxx@gmail.com>
2021-08-01 16:25:02 +0900

    fix dump1090-fa images.

--- dump1090-fa/run.sh
+++ dump1090-fa/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
 
-nginx -g 'pid /tmp/nginx.pid' -c '/nginx.conf'
+nginx -g 'pid /tmp/nginx.pid;' -c '/nginx.conf'
 mkdir -p '/run/dump1090'
 /usr/local/bin/dump1090 --quiet --net --write-json '/run/dump1090'
